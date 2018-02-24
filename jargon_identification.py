2082599938b76eeca49af41de82547bb75641086
Josh Miller <joshm@ccs.neu.edu>
2018-02-24 09:55:28 -0500

    tab fixes

--- jargon_identification.py
+++ jargon_identification.py
@@ -16,49 +16,49 @@ import stanford_corenlp_python_wrapper as nlp
 def files_to_texts(files):
 	texts = []
 	for i in range(len(files)):
-        file = files[i]
-        text = (open(file, 'r')).read
+		file = files[i]
+		text = (open(file, 'r')).read
 		texts.append(text)
 	return texts
 
 # nltk stemmer
 def get_stems(input):
-    tokenized = nltk.word_tokenize(input)
-    stems = []
-    for token in tokenized:
-        stems.append(PorterStemmer().stem(token))
-    return stems
+	tokenized = nltk.word_tokenize(input)
+	stems = []
+	for token in tokenized:
+		stems.append(PorterStemmer().stem(token))
+	return stems
 
 
 # sklearn tfidf
 def train_tfidf(stems):
-    tfidf = TfidfVectorizer(tokenizer=get_stems, stop_words='english')
-    ret = tfidf.fit_transform(stems.values())
-    print "RET:\n", ret
-    print "TFIDF features:\n", tfidf.get_feature_names() # TODO remove print statements
+	tfidf = TfidfVectorizer(tokenizer=get_stems, stop_words='english')
+	ret = tfidf.fit_transform(stems.values())
+	print "RET:\n", ret
+	print "TFIDF features:\n", tfidf.get_feature_names() # TODO remove print statements
 	print "TFIDF full:\n", tfidf
 
 def get_tfidf(tfidf, word):
-    token = nltk.word_tokenize(word)
-    stem = PorterStemmer().stem(token)
-    # TODO return value of tfidf(stem) from tfidf matrix
+	token = nltk.word_tokenize(word)
+	stem = PorterStemmer().stem(token)
+	# TODO return value of tfidf(stem) from tfidf matrix
 
 
 def count_coreferences(phrase, context):
-    # TODO count coreferences of phrase in context
-    return
+	# TODO count coreferences of phrase in context
+	return
 
 
 def main():
-    # hardcoded test data
-    files = ["practice_data/train_data_1.txt", "practice_data/train_data_2.txt", "practice_data/train_data_3.txt"]
-    texts = files_to_texts(files)
+	# hardcoded test data
+	files = ["practice_data/train_data_1.txt", "practice_data/train_data_2.txt", "practice_data/train_data_3.txt"]
+	texts = files_to_texts(files)
 	stems = []
 	for text in texts:
 		stems.extend(get_stems(text))
-    model = train_tfidf(stems)
+	model = train_tfidf(stems)
 	print "done"
 
 
 if __name__ == "__main__":
-    main()
+	main()
